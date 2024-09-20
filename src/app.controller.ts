cdeaa036cf13e2e04f57a7ade63a315b2155d109
NguynThuHigh <minhnguyen11a1cmg@gmail.com>
2024-09-20 18:04:08 +0700

    fix: [eslint] Delete `CR` [prettier/prettier]

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Res } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { Response } from 'express';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get(':id')
+  getHello(@Param('id') id: string, @Res() res: Response) {
+    res.status(200).json({ data: id });
   }
 }
