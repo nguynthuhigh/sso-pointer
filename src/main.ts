425230b3acd92572166582e12715e832e6f6ae1b
NguynThuHigh <minhnguyen11a1cmg@gmail.com>
2024-09-23 23:59:25 +0700

    12:59 pm still fix bug damn

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,19 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { HttpExceptionFilter } from './common/filter/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
+  const options = {
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders:
-      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-  });
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  };
+  app.enableCors(options);
   await app.listen(process.env.PORT);
 }
 bootstrap();
